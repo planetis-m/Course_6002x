23354a2231c56fba0f05457327e9b28b16bef8c1
Antonis G <capoiosct@gmail.com>
2016-11-24 13:38:56 +0200

    Try building my own data structures

--- Week1/food.nim
+++ Week1/food.nim
@@ -1,31 +1,32 @@
-import tables, sequtils
-
 type
+  Menu = object
+    foods: seq[Food]
   Food = object
     name: string
-    value, calories: int
-
-proc density(f: Food): float =
-  f.value / f.calories
 
 proc `$`(f: Food): string =
-  f.name & ": <" & $f.value & ", " & $f.calories & ">"
+  f.name & ": <, >"
 
-proc newFood(n: string, val, cal: int): Food =
-  result = Food(name: n, value: val, calories: cal)
+proc getFood(n: seq[string]): auto =
+  return iterator: Food =
+    for i in 0 .. high(n):
+      yield Food(name: n[i])
 
-proc buildMenu(items: Table[string, tuple[a, b: int]]): seq[Food] =
-  let m = len(items)
-  result = newSeq[Food](m)
-  for i, v in items.pairs:
-    let t = newFood(i, v.a, v.b)
-    result.add(t)
+proc initMenu(size: int): Menu =
+  newSeq(result.foods, size)
+
+proc toMenu(n: seq[string]): Menu =
+  result = initMenu(n.len)
+  let foods = getFood(n)
+  for i in 0 .. high(n):
+    let t = foods()
+    if finished(foods):
+      break
+    result.foods[i] = t
+
 
 let names = @["wine", "beer", "pizza", "burger", "fries",
               "cola", "apple", "donut"]
 
-let values = @[89,90,95,100,90,79,50,10]
-let calories = @[123,154,258,354,365,150,95,195]
-
-let items = zip(names, zip(values, calories)).toTable
-let foods = buildMenu(items)
+let m = toMenu(names)
+echo m.foods
