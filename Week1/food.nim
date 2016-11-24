6d367bf432045bed5c7ac678c71f13b2e29c368d
Antonis G <capoiosct@gmail.com>
2016-11-24 15:36:50 +0200

    If only there was a reason for such an abstruction

--- Week1/food.nim
+++ Week1/food.nim
@@ -21,11 +21,6 @@ proc `$`(f: Food): string =
 proc newFood(n: string, val, cal: int): Food =
   result = Food(name: n, value: val, calories: cal)
 
-proc getFoods(n: seq[string]; val, cal: seq[int]): auto =
-  return iterator: Food =
-    for i in 0 .. high(n):
-      yield newFood(n[i], val[i], cal[i])
-
 # -------------
 # Menu routines
 # -------------
@@ -36,9 +31,8 @@ proc initMenu(size = 0): Menu =
 proc toMenu(n: seq[string]; val, cal: seq[int]): Menu =
   let m = min(n.len, val.len, cal.len)
   result = initMenu(m)
-  let foods = getFoods(n, val, cal)
   for i in 0 .. <m:
-    result[i] = foods()
+    result[i] = newFood(n[i], val[i], cal[i])
 
 # -----------
 # Course Code
@@ -51,4 +45,3 @@ let values = @[89,90,95,100,90,79,50,10]
 let calories = @[123,154,258,354,365,150,95,195]
 
 let menu = toMenu(names, values, calories)
-echo menu
