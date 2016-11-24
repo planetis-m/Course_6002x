4c26a20f77b9cd273c5489b410c7aa757744ba73
Antonis G <capoiosct@gmail.com>
2016-11-24 22:36:53 +0200

    Finish this example

--- Week1/food.nim
+++ Week1/food.nim
@@ -1,3 +1,4 @@
+import algorithm
 # ----------------
 # type definitions
 # ----------------
@@ -12,14 +13,20 @@ type
 # Food routines
 # -------------
 
-proc density(f: Food): float =
-  f.value / f.calories
+proc density(f, j: Food): int =
+  f.value * j.calories - j.value * f.calories
 
 proc `$`(f: Food): string =
   f.name & ": <" & $f.value & ", " & $f.calories & ">"
 
 proc newFood(n: string, val, cal: int): Food =
   result = Food(name: n, value: val, calories: cal)
+
+proc getValue(f: Food): int =
+  result = f.value
+
+proc getCost(f: Food): int =
+  result = f.calories
 
 # -------------
 # Menu routines
@@ -35,13 +42,43 @@ proc toMenu(n: seq[string]; val, cal: seq[int]): Menu =
     result[i] = newFood(n[i], val[i], cal[i])
 
 # -----------
+# Greedy impl
+# -----------
+
+proc greedy(items: Menu, maxCost: int, cmp: proc(x, y: Food): int): (Menu, int) =
+  var itemsCopy = sorted(items, cmp, SortOrder.Descending)
+  var result = initMenu()
+  var totalValue, totalCost = 0
+  for i in itemsCopy:
+    if (totalCost + i.getCost()) <= maxCost:
+      result.add(i)
+      totalCost += i.getCost()
+      totalValue += i.getValue()
+  return (result, totalValue)
+
+proc testGreedy(items: Menu, constraint: int, cmp: proc(x, y: Food): int) =
+  let (taken, val) = greedy(items, constraint, cmp)
+  echo("Total value of items taken = ", val)
+  for item in taken:
+    echo("   ", item)
+
+proc testGreedys(foods: Menu, maxUnits: int) =
+  echo("Use greedy by value to allocate ", maxUnits, " calories")
+  testGreedy(foods, maxUnits, proc(x, y: Food): int = getValue(x) - getValue(y))
+  echo("\nUse greedy by cost to allocate ", maxUnits, " calories")
+  testGreedy(foods, maxUnits, proc(x, y: Food): int = getCost(y) - getCost(x))
+  echo("\nUse greedy by density to allocate ", maxUnits, " calories")
+  testGreedy(foods, maxUnits, density)
+
+# -----------
 # Course Code
 # -----------
 
 let names = @["wine", "beer", "pizza", "burger", "fries",
               "cola", "apple", "donut", "cake"]
 
-let values = @[89,90,95,100,90,79,50,10]
-let calories = @[123,154,258,354,365,150,95,195]
+let values = @[89, 90, 95, 100, 90, 79, 50, 10]
+let calories = @[123, 154, 258, 354, 365, 150, 95, 195]
 
-let menu = toMenu(names, values, calories)
+let foods = toMenu(names, values, calories)
+testGreedys(foods, 1000)
