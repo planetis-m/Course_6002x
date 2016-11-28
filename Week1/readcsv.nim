01bad071060884cdb60ae72ecc6a7d63127e363b
Antonis G <capoiosct@gmail.com>
2016-11-28 22:37:03 +0200

    fourth version

--- Week1/readcsv.nim
+++ Week1/readcsv.nim
@@ -1,12 +1,25 @@
 import strutils
 
-const data = slurp"card.csv"
+const data = "card.csv"
 
 type
   Card = seq[string]
 
-proc processCsvFile(): Card =
-  result = data.split({','} + NewLines)
-  discard result.pop()
+template withFile(f, fn, actions: untyped): untyped =
+  var f: File
+  if open(f, fn):
+    try:
+      actions
+    finally:
+      close(f)
+  else:
+    quit("cannot open: " & fn)
 
-const card = processCsvFile()
+proc processCsvFile(): Card =
+  withFile(fs, data):
+    let s = readAll(fs)
+    result = s.split({','} + NewLines)
+    discard result.pop()
+
+let card = processCsvFile()
+echo card
