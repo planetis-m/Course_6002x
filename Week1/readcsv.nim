724b7383491c0776b47c2524b910ee1a97a03fe3
Antonis G <capoiosct@gmail.com>
2016-11-28 21:43:43 +0200

    second version

--- Week1/readcsv.nim
+++ Week1/readcsv.nim
@@ -1,23 +1,32 @@
 import streams
 
-const filename = slurp"card.csv"
+const filename = "card.csv"
 
 type
   Card[W: static[int]] =
     array[1..W, string]
 
-
+proc `$`[T](a: openarray[T]): string =
+  ## The `$` operator for arrays.
+  result = "["
+  for val in a:
+    if result.len > 1: result.add(", ")
+    result.add($val)
+  result.add("]")
 
 proc processCsvFile(W: static[int]): Card[W] =
-  let ss = newStringStream(filename)
+  let fs = newFileStream(filename, fmRead)
+  if isNil(fs): quit("cannot open the file" & filename)
+
   var
     pos = 4
     index = 1
-  while not ss.atEnd():
-    result[index] = ss.readStr(4)
-    ss.setPosition(pos + 1)
+  while not fs.atEnd():
+    result[index] = fs.readStr(4)
+    fs.setPosition(pos + 1)
     inc(pos, 5)
     inc(index)
-  ss.close()
+  fs.close()
 
-let card = processCsvFile(10*10)
+let card = processCsvFile(100)
+echo card
