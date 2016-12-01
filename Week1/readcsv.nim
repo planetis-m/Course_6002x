04e58abb07435bcbf3d1507dcee6702421ea9afd
Antonis G <capoiosct@gmail.com>
2016-12-01 23:53:30 +0200

    Write data to binary file

--- Week1/readcsv.nim
+++ Week1/readcsv.nim
@@ -1,16 +1,31 @@
-import strutils, os
-
-const data = slurp"card.csv"
-
 type
-  Card = seq[string]
+  Item = ptr array[0..3, char]
 
-proc processCsvFile(): Card =
-  result = data.split({','} + NewLines)
-  discard result.pop()
+var ni: array[0..99, string] =
+  ["PVZW","OGVB","FYGN","NRWG","MDKW","FSGE","ROGE","FSOM","WUTK","RTKU",
+  "NUMS","QREN","VCRU","OPDE","PRTN","TAQN","HFUE","LTEC","RTCU","LMQD",
+  "DYHJ","NFEJ","BPJG","YTCB","OSYN","WOUT","VLNE","UIOJ","BETI","JORA",
+  "YOEK","LXFU","UJYW","IYZE","SCUE","QVCL","GVIU","JXOZ","JLKF","DPJW",
+  "DJSL","THQN","OUST","GPBC","FYLJ","QHYV","TVMA","FYDJ","USDP","QLGX",
+  "PNQB","LXSE","JYKE","CNLJ","FCLE","SBMD","UTWI","DKVW","LESI","ANVQ",
+  "AIYU","VXBQ","TGNK","UZYC","SWIY","YJGS","FYEG","FTZW","GLFC","CSLD",
+  "RUOB","TJQE","YUFA","EITH","RPYW","PRBM","WKEL","MHCR","FOTG","AXZI",
+  "WMOQ","DQXI","YLOE","ZFPA","SXNK","OTRM","TVIP","BHZI","MBTJ","DMCJ",
+  "XDRT","MTGQ","RKHQ","FTPB","XNAK","MUNH","UDZF","CQOF","UPVL","EGFZ"]
 
-let card = processCsvFile()
+proc ffi(s: var string): Item =
+  cast[Item](addr s[0])
+
+proc ffi[T](a: var array[T, string]): array[T, Item] =
+  for i in 0..len(a)-1:
+    result[i] = ffi(a[i])
+
+var n = ffi(ni)
 
 var f = open("data", fmWrite)
-discard f.writeBuffer(addr data[0], len(data) * sizeof(data[0]))
+
+let length = len(n) * sizeof(n[0])
+if f.writeBuffer(addr n[0], length) != length:
+  echo "Error: Could not write binary file"
+
 close(f)
