bd44e0aeec506c28a4cc54de88ab0265245fc4d1
Antonis G <capoiosct@gmail.com>
2016-12-01 16:35:20 +0200

    Embed file and write data in binary

--- Week1/readcsv.nim
+++ Week1/readcsv.nim
@@ -1,25 +1,16 @@
-import strutils
+import strutils, os
 
-const data = "card.csv"
+const data = slurp"card.csv"
 
 type
   Card = seq[string]
 
-template withFile(f, fn, actions: untyped): untyped =
-  var f: File
-  if open(f, fn):
-    try:
-      actions
-    finally:
-      close(f)
-  else:
-    quit("cannot open: " & fn)
-
 proc processCsvFile(): Card =
-  withFile(fs, data):
-    let s = readAll(fs)
-    result = s.split({','} + NewLines)
-    discard result.pop()
+  result = data.split({','} + NewLines)
+  discard result.pop()
 
 let card = processCsvFile()
-echo card
+
+var f = open("data", fmWrite)
+discard f.writeBuffer(addr data[0], len(data) * sizeof(data[0]))
+close(f)
