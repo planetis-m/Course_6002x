48688b16102d7f5eb644aaed5b445726ff03ac6c
Antonis G <capoiosct@gmail.com>
2017-01-30 00:10:02 +0200

    Add multiplot \Doesn't work as expected due to wrapper logic

--- Week2/plot_example.nim
+++ Week2/plot_example.nim
@@ -1,29 +1,19 @@
-import gnuplot, math
+import gnuplot, math, sequtils
 
-var
-  samples = newSeq[float]()
-  linear = newSeq[float]()
-  quadratic = newSeq[float]()
-  cubic = newSeq[float]()
-  exponential = newSeq[float]()
+let
+  samples = toSeq(0..29)
+  linear = toSeq(0..29)
+  quadratic = toSeq(0..29).mapIt(it^2)
+  cubic = toSeq(0..29).mapIt(it^3)
+  exponential = toSeq(0..29).mapIt(pow(1.5, it.float))
 
-iterator rangeFloat(a, b: float, step = 1.0): float =
-  var res = a
-  while res <= b:
-    yield res
-    res += step
-
-
-for i in rangeFloat(0.0, 29.0):
-  samples.add i
-  linear.add i
-  quadratic.add pow(i, 2.0)
-  cubic.add pow(i, 3.0)
-  exponential.add pow(1.5, i)
+cmd "set multiplot layout 2, 2"
 
 plot(samples, linear, "Linear Plot")
 plot(samples, quadratic, "Quadratic Plot")
 plot(samples, cubic, "Cubic Plot")
 plot(samples, exponential, "Exponential Plot")
+
+cmd "unset multiplot"
 
 discard readChar stdin
