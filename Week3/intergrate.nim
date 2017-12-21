a5617f53630e62da5937978d009d6ddbb49e06ca
Antonis G <capoiosct@gmail.com>
2017-12-21 15:54:34 +0200

    Do without lenientops

--- Week3/intergrate.nim
+++ Week3/intergrate.nim
@@ -1,4 +1,4 @@
-import math, random, strutils, lenientops
+import math, random, strutils
 
 type
    Function = proc(x: float): float
@@ -11,22 +11,22 @@ proc simpson(f: Function, x, h: float): float =
    result = (f(x) + 4.0*f(x+h/2.0) + f(x+h)) / 6.0
 
 proc integrate(f: Function; a, b: float; steps: int; meth: Rule): float =
-   let h = (b-a) / float(steps)
+   let h = (b - a) / float(steps)
    for i in 0 ..< steps:
-      result += meth(f, a+float(i)*h, h)
+      result += meth(f, a + float(i)*h, h)
    result = h * result
 
-proc gaussian(x: float; mu, sigma: int): float =
+proc gaussian(x, mu, sigma: float): float =
    let factor1 = 1.0/(sigma*sqrt(2.0*Pi))
    let factor2 = exp(-pow(x-mu, 2.0)/(2.0*sigma*sigma))
    result = factor1 * factor2
 
 proc checkEmpirical(numTrials: int) =
    template ff(f: float): string = formatFloat(f, ffDecimal, 4)
-   template rand(a, b: int): int = rand(b - a) + a
+   template randz(a, b: int): float = float(rand(b - a) + a)
    for t in 1 .. numTrials:
-      let mu = rand(-10, 10)
-      let sigma = rand(1, 10)
+      let mu = randz(-10, 10)
+      let sigma = randz(1, 10)
       echo("For mu = ", mu, " and sigma = ", sigma)
       for numStd in [1.0, 1.96, 3.0]:
          let area = integrate(proc (x: float): float = gaussian(x, mu, sigma),
